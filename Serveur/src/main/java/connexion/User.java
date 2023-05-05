ea7f7f93b0c9c163e31c06a499334df5b5d84104
Hanybay <hanybayazid11@gmail.com>
2023-05-05 15:05:45 +0200

    Ajout du début de la javadoc pour la classe utilisateur

--- Serveur/src/main/java/connexion/User.java
+++ Serveur/src/main/java/connexion/User.java
@@ -9,6 +9,9 @@ import java.util.HashMap;
 import java.sql.Date;
 import com.mysql.cj.xdevapi.Result;
 
+import communication.AttackInformations;
+import inside.Board;
+
 public class User {
 	/**
 	 * Identifiant de l'utilisateur
@@ -30,19 +33,73 @@ public class User {
 	private String password;
 	
 	// Getters
+	
+	/**
+	 * Retourne l'identifiant de l'utilisateur
+	 * @return identifiant de l'utilisateur
+	 * 
+	 * @see User#id
+	 */
+	
 	public int getId() {return id;}
+	
+	/**
+	 * Retourne la position de l'utilisateur
+	 * @return la position de l'utilisateur
+	 * 
+	 * @see User#position
+	 */
 	public int getposition() {return position;}
+	
+	/**
+	 * Retourne le pseudo de l'utilisateur
+	 * @return le pseudo de l'utilisateur
+	 * 
+	 * @see User#pseudo
+	 */
 	public String getPseudo() {return pseudo;}
+	
+	/**
+	 * Retourne le mot de passe de l'utilisateur
+	 * @return le mot de passe de l'utilisateur
+	 * 
+	 * @see User#password
+	 */
 	public String getPassword() {return password;}
 
 	// Setters
+	
+	/**
+	 * Change la valeur de l'identifiant de l'utilisateur
+	 * @param le nouvel identifiant de l'utilisateur
+	 * 
+	 * @see User#id
+	 */
 	public void setId(int id) {this.id = id;}
+	
+	/**
+	 * Change la valeur de la position du curseur de l'utilisateur
+	 * @param la nouvelle position du curseur de l'utilisateur
+	 * 
+	 * @see User#position
+	 */
 	public void setPosition(int position) {this.position = position;}
+
+	/**
+	 * Change le pseudo de l'utilisateur
+	 * @param le nouveau pseudo de l'utilisateur
+	 * 
+	 * @see User#pseudo
+	 */
 	public void setPseudo(String pseudo) {this.pseudo = pseudo;}
+
+	/**
+	 * Change le mot de passe de l'utilisateur
+	 * @param le nouveau mot de passe de l'utilisateur
+	 * 
+	 * @see User#password
+	 */
 	public void setPassword(String password) {this.password = password;}
-	
-	
-	
 	
 	
 }
