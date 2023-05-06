4ff9c77c5899d172ce7fa2587ac740e8883e6e7a
Hanybay <hanybayazid11@gmail.com>
2023-05-06 19:15:51 +0200

    Surcharge de la méthode init pour la connexion TCP

--- Serveur/src/main/java/connexion/ParamBD.java
+++ Serveur/src/main/java/connexion/ParamBD.java
@@ -1,6 +1,13 @@
 package connexion;
 
 import javax.servlet.ServletContext;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class ParamBD {
 
@@ -8,7 +15,7 @@ public class ParamBD {
 	protected static String bdLogin;
 	protected static String bdPassword;
 
-	protected static void init(ServletContext context) {
+	public static void init(ServletContext context) {
 		try {
 			Class.forName(context.getInitParameter("JDBC_DRIVER"));
 			bdURL = context.getInitParameter("JDBC_URL");
@@ -19,4 +26,44 @@ public class ParamBD {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void init(String xmlFileName) {
+	    try {
+	        File inputFile = new File(xmlFileName);
+	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	        Document doc = dBuilder.parse(inputFile);
+	        doc.getDocumentElement().normalize();
+
+	        NodeList nodeList = doc.getElementsByTagName("context-param");
+
+	        for (int i = 0; i < nodeList.getLength(); i++) {
+	            Node node = nodeList.item(i);
+	            if (node.getNodeType() == Node.ELEMENT_NODE) {
+	                Element element = (Element) node;
+	                String paramName = element.getElementsByTagName("param-name").item(0).getTextContent();
+	                String paramValue = element.getElementsByTagName("param-value").item(0).getTextContent();
+	                switch (paramName) {
+	                    case "JDBC_DRIVER":
+	                        Class.forName(paramValue);
+	                        break;
+	                    case "JDBC_URL":
+	                        bdURL = paramValue;
+	                        break;
+	                    case "JDBC_LOGIN":
+	                        bdLogin = paramValue;
+	                        break;
+	                    case "JDBC_PASSWORD":
+	                        bdPassword = paramValue;
+	                        break;
+	                }
+	            }
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	}
+
 }
+
