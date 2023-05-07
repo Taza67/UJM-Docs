99929877eb28f4abb7913d7ef3b226d7069aeb29
Hanybay <hanybayazid11@gmail.com>
2023-05-07 15:30:30 +0200

    Ajout de la méthode isGuest dans la classe User

--- Serveur/src/main/java/connexion/DocManager.java
+++ Serveur/src/main/java/connexion/DocManager.java
@@ -1,5 +1,20 @@
 package connexion;
 
+import connexion.document.Document;
+
+/**
+ * Cette classe représente le gestionnaire des documents qui permet la modification des documents d'une façon synchronisée
+ * @author BAYAZID Hany
+ */
+
+
 public class DocManager {
+	/**
+	 * Document à modifier
+	 */
+	private Document d;
 	
+
+
+
 }
